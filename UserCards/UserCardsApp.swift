f4b4481ab2843d9214540216a8a27f2c98f8ac79
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-19 11:37:51 -0600

    Initial Commit

---
+++ UserCards/UserCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserCardsApp.swift
+//  UserCards
+//
+//  Created by Benjamin Rojo on 19/05/25.
+//
+
+import SwiftUI
+
+@main
+struct UserCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
