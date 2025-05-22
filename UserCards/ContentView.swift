66f4ae85b02be089aabc9ff86e09d18bcf0bed4f
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-21 19:41:19 -0600

    i change some code

--- UserCards/ContentView.swift
+++ UserCards/ContentView.swift
@@ -8,49 +8,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
+        ZStack{
             LinearGradient(
-                gradient: Gradient(colors: [.blue, .white]),
+                gradient: Gradient(colors: [.green, .gray]),
                 startPoint: .top,
                 endPoint: .bottom
             )
             .ignoresSafeArea()
-
-            VStack {
-                Text("Mi rutina")
-                    .foregroundColor(.red)
+            VStack{
+                Text("Agenda de Hoy")
+                    .foregroundColor(.yellow)
                     .font(.title)
-                ActivityRowView()
+                EventoView()
             }
         }
     }
 }
 
-struct ActivityRowView: View{
-    var body: some View{
-        VStack{
-            RoutineDetailView(actividad: "despertar", hora: "8:00am", icono :  "star.fill")
-            RoutineDetailView(actividad: "desayuno", hora: "9:00am", icono :  "star.fill")
-            RoutineDetailView(actividad: "estudio", hora: "10:00am", icono : "star.fill")
-            RoutineDetailView(actividad: "como", hora: "4:00pm", icono : "star.fill")
-            RoutineDetailView(actividad: "free time", hora: "5:00pm", icono :  "star.fill")
-        }
-    }
-}
-
-struct RoutineDetailView: View{
-    let actividad : String
+struct EventRowView: View{
     let hora : String
-    let icono : String
-    
+    let tipo: String
+    let lugar: String
+    let icono: String
     var body: some View{
-        VStack{
-            Text(actividad)
+        HStack{
             Text(hora)
+            Spacer()
+            Text(tipo)
+            Spacer()
+            Text(lugar)
+            Spacer()
             Image(systemName: icono)
         }
     }
 }
+
+struct EventoView: View{
+    var body: some View{
+        VStack{
+            EventRowView( hora: "20:00hrs", tipo: "Conferencia", lugar: "CDMX", icono: "star.fill")
+            EventRowView( hora: "10:00hrs", tipo: "Conferencia", lugar: "CDMX", icono: "star.fill")
+            EventRowView( hora: "5:00hrs", tipo: "Conferencia", lugar: "CDMX", icono: "star.fill")
+        }
+       
+    }
+}
+
 
 
 #Preview {
