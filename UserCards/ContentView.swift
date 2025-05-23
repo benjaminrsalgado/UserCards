23f7c4af7f5e491afa8a9dfd02e67be1b45437ce
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-23 12:21:43 -0600

    i did a list of movies

--- UserCards/ContentView.swift
+++ UserCards/ContentView.swift
@@ -9,49 +9,50 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            Color.blue.ignoresSafeArea()
+            LinearGradient(
+                    gradient: Gradient(colors: [.black, .gray]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                ).ignoresSafeArea()
             VStack{
-                Text("Mi viaje")
-                    .foregroundColor(.white)
+                Text("Mis peliculas favoritas")
                     .font(.title)
-                Itinerario()
+                    .foregroundColor(.white)
+                PeliculasView()
+            }
             }
             }
             
         }
+struct PeliculasView: View{
+    var body: some View{
+        ScrollView(.horizontal){
+            HStack{
+                PeliculaCardView(nombre: "Harry Potter", icono: "star.fill")
+                PeliculaCardView(nombre: "The Hobbit", icono: "star.fill")
+                PeliculaCardView(nombre: "Lord of the Rings", icono: "star.fill")
+            }
+            .padding()
+        }
     }
+}
 
-struct TravelDayView : View{
-    let dia: String
-    let ciudad: String
+struct PeliculaCardView: View{
+    let nombre: String
     let icono: String
     var body: some View{
-        HStack{
-            Text(dia)
-            Text(ciudad)
-            Image(systemName: icono)
+        VStack{
+            Text(nombre)
+            Image(systemName:icono)
         }
         .padding()
+        .background(.orange)
+        .cornerRadius(22)
     }
 }
 
-struct Itinerario: View{
-    var body: some View{
-            ScrollView(.horizontal) {
-                HStack{
-                TravelDayView(dia: "1 diciembre", ciudad: "Roma",icono:  "star.fill")
-                TravelDayView(dia: "2 diciembre", ciudad: "Finlandia",icono:  "star.fill")
-                TravelDayView(dia: "3 diciembre", ciudad: "España",icono:  "star.fill")
-                TravelDayView(dia: "4 diciembre", ciudad: "Portugal",icono:  "star.fill")
-                TravelDayView(dia: "5 diciembre", ciudad: "Sweeden",icono:  "star.fill")
-            }
-            .padding(.horizontal)
-            .foregroundColor(.orange)
-            .bold()
-        }
-        
-    }
-}
+
+
 
 
 
